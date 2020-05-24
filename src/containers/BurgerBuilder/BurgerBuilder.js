28c35ad61c34516051e1fd3b0cb0ca8a57cb2d65
robert_coulston <robert_coulston@hotmail.com>
2020-05-25 08:53:22 +1000

    Order summary completed

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -94,6 +94,7 @@ class BurgerBuilder extends Component {
             ingredients={this.state.ingredients}
             cancelled={() => this.purchaseCancelledHandler()}
             purchased={() => this.purchaseContinueHandler()}
+            price= {this.state.totalPrice}
           />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
